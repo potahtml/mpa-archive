039ab3474207cb6892fceaec9e172575e269ea74
Tito Bouzout <tito.bouzout@gmail.com>
2024-06-11 08:24:35 -0300

    when a file is not found fetch it from the site and update the zip file

--- src/server.js
+++ src/server.js
@@ -16,6 +16,8 @@ console.log()
 
 const cwd = process.cwd()
 
+const timeouts = {}
+
 fs.promises
 	.readdir(cwd)
 	.then(files => files.map(file => /.zip$/.test(file) && serve(file)))
@@ -25,13 +27,13 @@ function serve(zipFile) {
 
 	zipFile = cwd + '/' + zipFile
 
-	const zip = new AdmZip(zipFile)
+	let zip = new AdmZip(zipFile)
 
-	const entries = zip.getEntries()
+	let entries = zip.getEntries()
 
 	const port = seededRandom(1025, 65534, zipFile)
 
-	const server = http.createServer(function (req, res) {
+	const server = http.createServer(async function (req, res) {
 		const path = getRequestPathFromPath(req.url)
 		const pathNoQuery = getRequestPathWithoutQueryStringFromPath(
 			req.url,
@@ -47,24 +49,38 @@ function serve(zipFile) {
 			pathNoQuery + '/index.html',
 		].map(s => s.replace(/^\/+/, ''))
 
+		function serve(file, path, data) {
+			res.setHeader('Content-Type', contentType(file))
+			res.setHeader('Pragma', 'public')
+			res.setHeader('Cache-Control', 'public, max-age=180')
+			res.writeHead(200)
+			res.end(Buffer.from(data))
+			console.log(
+				`🍽  ${domain}/${file === path ? path : `${path} -> /${file}`}`,
+			)
+		}
+		function notfound(path) {
+			res.setHeader('Content-Type', 'text/plain; charset=utf-8')
+			res.setHeader('Pragma', 'public')
+			res.setHeader('Cache-Control', 'public, max-age=180')
+			res.writeHead(404)
+			res.end(`404 Not Found: ${domain}/${path}`)
+			console.log(`❌ ${domain}/${path}`)
+		}
+
 		for (const file of tryFiles) {
 			const entry = entries.find(
 				zipEntry => zipEntry.entryName == file,
 			)
 
 			if (entry) {
-				res.setHeader('Content-Type', contentType(file))
-				res.setHeader('Pragma', 'public')
-				res.setHeader('Cache-Control', 'public, max-age=180')
-				res.writeHead(200)
-				res.end(entry.getData())
-				console.log(
-					`🍽  ${domain}/${file === path ? path : `${path} -> /${file}`}`,
-				)
+				serve(file, path, entry.getData().slice(0))
+
 				return
 			}
 		}
 
+		// index Of
 		if (path === '' || path === '/' || path === '/index.html') {
 			let content = '<h1>index of ' + domain + '</h1><ul>'
 			for (let entry of entries) {
@@ -84,12 +100,37 @@ function serve(zipFile) {
 			res.end(content)
 			return
 		}
-		res.setHeader('Content-Type', 'text/plain; charset=utf-8')
-		res.setHeader('Pragma', 'public')
-		res.setHeader('Cache-Control', 'public, max-age=180')
-		res.writeHead(404)
-		res.end(`404 Not Found: ${domain}/${path}`)
-		console.log(`❌ ${domain}/${path}`)
+
+		// 404
+		console.log(`🔗 ${domain}/${path}`)
+
+		// fetch it from the server
+		fetch(`https://${domain}/${path}`)
+			.then(async response => {
+				if (response.status >= 400) {
+					console.log(`⚠ ${response.status} ${domain}/${path}`)
+					notfound(path)
+				} else {
+					const result = await response.arrayBuffer()
+					zip.addFile(path, Buffer.from(result.slice(0), 'binary'))
+
+					console.log(`✔  ${domain}/${path}`)
+
+					clearTimeout(timeouts[domain])
+					timeouts[domain] = setTimeout(() => {
+						zip.writeZip(zipFile)
+						// if we dont reopen it then it crashes with `getEntries`
+						zip = new AdmZip(zipFile)
+						entries = zip.getEntries()
+					}, 5000)
+
+					serve(path, path, result.slice(0))
+				}
+			})
+			.catch(e => {
+				console.log(e)
+				notfound(path)
+			})
 	})
 
 	server.on('error', () => {})
